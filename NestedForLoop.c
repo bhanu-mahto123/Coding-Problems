14039977d1b68c725b6fedf005c56cd85c0414e0
bhanu-mahto123 <mahtobhanu500@gmail.com>
2021-01-25 00:20:18 +0530

    Create NestedForLoop.c
    Brute Force for finding all possible combination of placing N queens in the i th column where 1 denotes placing of queen.
    https://www.geeksforgeeks.org/n-queen-problem-backtracking-3/

---
+++ NestedForLoop.c
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#define MAX 8
+int board[MAX][MAX];
+void printAllComb(int Level,int N)
+{
+    if(Level >= N){
+        for(int i=0;i<N;i++)
+        {
+              for(int j=0;j<N;j++){
+                  printf("%d ",board[i][j]);
+              }
+              printf("\n");
+        }
+        printf("\n#Printed Conf for %d #\n",Level);
+        return;
+    }
+    for(int i=0;i<N;i++){
+        board[i][Level] = 1;
+        printAllComb(Level+1,N);
+        board[i][Level] = 0;
+    }
+}
+
+int main()
+{
+    int N = 4;
+    for(int i=0;i<N;i++)
+        {
+              for(int j=0;j<N;j++){
+                  board[i][j] = 0;
+              }
+        }
+        printAllComb(0,N);
+    printf("Hello World");
+
+    return 0;
+}
